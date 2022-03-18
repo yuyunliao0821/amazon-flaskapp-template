da280ad9d9aec1117b1dfe274508a1ea56fe7fec
yuyunliao0821 <53509008+yuyunliao0821@users.noreply.github.com>
2022-03-18 17:01:18 +0800

    update html,js

--- static/js/predict_sentiment.js
+++ static/js/predict_sentiment.js
@@ -1,30 +1,32 @@
-function predictSentiment() {
+window.onload = function(){
+  function predictSentiment() {
   
-  // get elements from html DOM
-  var inputText = document.getElementById("textbox").value;
-  var selectedModel = document.getElementById('ml-model').value;
-  var predictionContainer = document.getElementById("prediction");
-  var predictionText = document.getElementById("prediction-text");
-  var originalText = document.getElementById("original-text")
-
-  predictionContainer.style.display = 'flex';
-  predictionText.style.display= 'None'
-
-  // Create variable for sending request to python
-  var serverData=[{
-    'text':inputText,
-    'mlmodel':selectedModel
-  }]
-
-  //Define ajax request here:
+    var inputText = document.getElementById("textbox").value;
+    var selectedModel = document.getElementById('ml-model').value;
+    var predictionContainer = document.getElementById("prediction");
+    var predictionText = document.getElementById("prediction-text");
+    var originalText = document.getElementById("original-text")
   
+    predictionContainer.style.display = 'flex';
+    predictionText.style.display= 'None'
+  
+    var serverData=[{
+      'text':inputText,
+      'mlmodel':selectedModel
+    }]
+  
+    // Define ajax request here:
 
 
-  //=======================================
- 
 
-  //  $.get("/predict?"+"text="+inputText+"&"+"mlmodel="+selectedModel).done(function (data) {
-  //    predictionText.innerHTML = data;
-  //  })
 
+    //=============================
+  
+    //  $.get("/predict?"+"text="+inputText+"&"+"mlmodel="+selectedModel).done(function (data) {
+    //    predictionText.innerHTML = data;
+    //  })
+  }
+  
+  var button = document.getElementById('button')
+  button.addEventListener('click', predictSentiment)
 }
